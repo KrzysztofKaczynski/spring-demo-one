cfef598f17548e035a2b3054fa8afeb763cd0dd8
Krzysztof Kaczyński <qris70@gmail.com>
2018-11-14 20:00:41 +0100

    [add] - BeanScopeDemoApp class.

---
+++ src/com/luv2code/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+/**
+ * com.luv2code.springdemo Created by MałyKriszo on 2018-11-14.
+ */
+public class BeanScopeDemoApp {
+
+    public static void main(String[] args) {
+
+    }
+}

e7ff8f75000b64150f8133b6553465580d56d2ac
Krzysztof Kaczyński <qris70@gmail.com>
2018-11-14 18:31:40 +0100

    [add] - SetterDemoApp class.

---
+++ src/com/luv2code/springdemo/SetterDemoApp.java
@@ -0,0 +1,27 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * com.luv2code.springdemo Created by MałyKriszo on 2018-11-14.
+ */
+public class SetterDemoApp {
+
+    public static void main(String[] args) {
+
+        // load the spring configuration file
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // retrieve bean from spring container
+        CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
+
+        // call methods on the bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        System.out.println(theCoach.getDailyFortune());
+
+        // close the context
+        context.close();
+    }
+}
